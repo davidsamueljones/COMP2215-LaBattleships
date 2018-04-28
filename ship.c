ddbc0dd91f362ae601ddc25ed332864bf2b77669
David Jones <davidsamueljones@icloud.com>
2018-04-28 19:09:56 +0100

    Added initial ship structures and methods
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

---
+++ ship.c
@@ -0,0 +1,136 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "ship.h"
+
+
+void print_ships(ship_t ships[], uint8_t count) {
+    printf("Ships:\n");
+    for (uint8_t ship = 0; ship < count; ship++) {
+        printf("  (%d) %-10s - X:%d, Y:%d, D:%d, L:%d, H:%d\n",
+               ships[ship].ref, ships[ship].name, ships[ship].x,
+               ships[ship].y, ships[ship].dir, ships[ship].length, ships[ship].hits);
+    }
+}
+
+bool auto_place_ships(grid_t* grid, ship_t ships[], uint8_t count) {
+    bool allplaced = false;
+    // Allocate data for auto grid placements
+    grid_t alloc_grid = {.width = grid->width, .height = grid->height};
+    allocate_grid_data(&alloc_grid, false);
+
+    if (alloc_grid.data != NULL) {
+        // Auto place ships
+        allplaced = true;
+        for (uint8_t ship = 0; ship < count; ship++) {
+            bool placed = auto_place_ship(grid, &alloc_grid, &ships[ship]);
+            allplaced &= placed;
+        };
+        free(alloc_grid.data);
+    }
+
+    return allplaced;
+}
+
+bool auto_place_ship(grid_t* ship_grid, grid_t* alloc_grid, ship_t* ship) {
+    uint16_t available = gen_availability_grid(ship_grid, alloc_grid, ship->length);
+    if (available > 0) {
+        uint16_t allocation = 1 + rand() % available;
+        if (allocate_ship_pos(alloc_grid, ship, allocation)) {
+            place_ship(ship_grid, ship, false);
+            return true;
+        }
+    }
+    return false;
+}
+
+bool allocate_ship_pos(grid_t* alloc_grid, ship_t* ship, uint16_t allocation) {
+    uint16_t ongoing = 0;
+    if (allocation > 0) {
+        for (ship->x = 0; ship->x < alloc_grid->width; ship->x++) {
+            for (ship->y = 0; ship->y < alloc_grid->height; ship->y++) {
+                g_data data = get_grid_data(alloc_grid, ship->x, ship->y);
+                for (ship->dir = D_North; ship->dir <= D_West; ship->dir++) {
+                    // Check if flag is set
+                    if (data & (1 << ship->dir)) {
+                        ongoing++;
+                        if (ongoing == allocation) {
+                            // Allocation found
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+uint16_t gen_availability_grid(grid_t* ship_grid, grid_t* alloc_grid, uint8_t length) {
+    uint16_t count = 0;
+    ship_t ship = {.length = length};
+    for (ship.x = 0; ship.x < ship_grid->width; ship.x++) {
+        for (ship.y = 0; ship.y < ship_grid->height; ship.y++) {
+            // Clear current allocation
+            int16_t pos = map_grid_pos(alloc_grid, ship.x, ship.y);
+            alloc_grid->data[pos] = 0;
+            for (ship.dir = D_North; ship.dir <= D_West; ship.dir++) {
+                // Set the direction bit high if placeable
+                if (place_ship_valid(ship_grid, &ship)) {
+                    alloc_grid->data[pos] |= 1 << ship.dir;
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+bool place_ship(grid_t* grid, ship_t* ship, bool verify) {
+    if (verify && !place_ship_valid(grid, ship)) {
+        return false;
+    }
+    int8_t x = ship->x;
+    int8_t y = ship->y;
+    for (uint8_t i = 0; i < ship->length; i++) {
+        set_grid_data(grid, x, y, (g_data) ship->ref);
+        move_x_y(&x, &y, ship->dir);
+    }
+    return true;
+}
+
+bool place_ship_valid(grid_t* grid, ship_t* ship) {
+    int8_t x = ship->x;
+    int8_t y = ship->y;
+    for (uint8_t i = 0; i < ship->length; i++) {
+        if (get_grid_data(grid, x, y) != EMPTY_POS) {
+            return false;
+        };
+        move_x_y(&x, &y, ship->dir);
+    }
+    return true;
+}
+
+bool is_ship_destroyed(ship_t* ship) {
+    return ship->length == ship->hits;
+}
+
+void move_x_y(int8_t* x, int8_t* y, dir_t dir) {
+    switch (dir) {
+    case D_North:
+        *y -= 1;
+        break;
+    case D_East:
+        *x += 1;
+        break;
+    case D_South:
+        *y += 1;
+        break;
+    case D_West:
+        *x -= 1;
+        break;
+    }
+}
+
+
---
+++ ship.h
@@ -0,0 +1,133 @@
+#ifndef SHIP_H
+#define SHIP_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "grid.h"
+
+/* Default clear values for non-property values */
+#define SHIP_DEFAULTS .hits = 0, .x = 0, .y = 0, .dir = D_South
+
+/**
+ * Enumeration of ship directions.
+ */
+typedef enum {
+    D_North,
+    D_East,
+    D_South,
+    D_West
+} dir_t;
+
+/**
+ * Structure holding a ships identifying data, positioning data and
+ * its current state.
+ */
+typedef struct {
+    char* name;
+    int8_t ref;
+    int8_t x;
+    int8_t y;
+    dir_t dir;
+    uint8_t length;
+    uint8_t hits;
+} ship_t;
+
+/**
+ * Print the states of the given ship list.
+ *
+ * @param ships Ship array
+ * @param count Number of ships in array
+ */
+void print_ships(ship_t ships[], uint8_t count);
+
+/**
+ * Using a grid that may be filled with ships as a basis, attempt to place the provided list of ships randomly.
+ * This requires a memory allocation equal to that of the provided grid.
+ * 
+ * @param  grid  Currently placed ships (no shot bits allowed to be set)
+ * @param  ships Ships to attempt to auto-place
+ * @param  count Number of ships in provided list
+ * @return       Whether all ships were placed
+ */
+bool auto_place_ships(grid_t* grid, ship_t ships[], uint8_t count);
+
+/**
+ * Using a grid filled with ships as a basis, attempt to place another ship randomly. If possible, the
+ * ship's positioning fields are set and the ship will be placed on the ship_grid.
+ *
+ * @param  ship_grid  Currently placed ships (no shot bits allowed to be set)
+ * @param  alloc_grid Grid with pre-allocated memory equal in size to ship_grid (will be cleared)
+ * @param  ship       Ship to attempt to auto-place
+ * @return            Whether ship could be auto-placed
+ */
+bool auto_place_ship(grid_t* ship_grid, grid_t* alloc_grid, ship_t* ship);
+
+/**
+ * Using an allocation grid, attempt to allocate a position and direction to a given ship.
+ * This is achieved by counting each set flag in the allocation grid as an available allocation,
+ * The selected flag is the n'th one (where n == allocation).
+ *
+ * @param  alloc_grid A pre-generated allocation grid
+ * @param  ship       The ship who's position should be updated
+ * @param  allocation Of the allocatable places, the one to select
+ * @return            Whether a position was successfully allocated, if this returns false
+ *                    the current state of the ship's position fields should be ignored
+ */
+bool allocate_ship_pos(grid_t* alloc_grid, ship_t* ship, uint16_t allocation);
+
+/**
+ * Using a grid filled with ships as a basis, generate another grid with flags set that represent which directions
+ * from the respective position support the given ship length. A position can have multiple flags set if multiple
+ * directions are supported
+ *
+ * Flags:
+ * 15-4 [NOTHING], 3 [West], 2 [South], 1 [East], 0 [North]
+ *
+ * @param ship_grid  Currently placed ships (no shot bits allowed to be set)
+ * @param alloc_grid Grid with pre-allocated memory equal in size to ship_grid (will be cleared)
+ * @param length     Length of space required for move to be available
+ * @return           The number of available placements
+ */
+uint16_t gen_availability_grid(grid_t* ship_grid, grid_t* alloc_grid, uint8_t length);
+
+/**
+ * First verify if the position is valid before placing a ship on the grid.
+ * The ship is placed as its reference field in all positions it covers.
+ *
+ * @param  grid   Grid configuration to update
+ * @param  ship   Ship configuration to use for update
+ * @param  verify Whether ship placement should be verified first
+ * @return        Whether the ship was placed
+ */
+bool place_ship(grid_t* grid, ship_t* ship, bool verify);
+
+/**
+ * Check if a ship can be placed in its stored position on the given grid.
+ * A ship must fit within the bounds of the grid and not cross any tile that is
+ * not empty.
+ *
+ * @param  grid Grid configuration to check on
+ * @param  ship Ship configuration to check with
+ * @return      Whether placement is valid
+ */
+bool place_ship_valid(grid_t* grid, ship_t* ship);
+
+/**
+ * Check if an input ship is in a destroyed state. A destroyed state is where the number of hits equals the
+ * number of hitpoints (length).
+ * 
+ * @param  ship Ship to check
+ * @return      Whether ship is in a destroyed state
+ */
+bool is_ship_destroyed(ship_t* ship);
+
+/**
+ * Shift an x y coordinate in a given direction.
+ * @param x   Pointer to x coordinate to shift
+ * @param y   Pointer to y coordinate to shift
+ * @param dir Direction in which to shift
+ */
+void move_x_y(int8_t* x, int8_t* y, dir_t dir);
+
+#endif // SHIP_H
