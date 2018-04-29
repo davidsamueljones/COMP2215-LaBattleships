921924039c66e527ed605f6af09ec38216c62fa9
David Jones <davidsamueljones@icloud.com>
2018-04-29 02:39:09 +0100

    Added backend OS files
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

---
+++ lafortuna/os.c
@@ -0,0 +1,24 @@
+/*  _____          _                      ___  ____
+ * |  ___|__  _ __| |_ _   _ _ __   __ _ / _ \/ ___|
+ * | |_ / _ \| '__| __| | | | '_ \ / _` | | | \___ \
+ * |  _| (_) | |  | |_| |_| | | | | (_| | |_| |___) |
+ * |_|  \___/|_|   \__|\__,_|_| |_|\__,_|\___/|____/
+ *
+ */
+
+
+#include "os.h"
+#include "rios/rios.h"
+#include "ruota/ruota.h"
+
+void os_init(void) {
+    /* 8MHz clock, no prescaling (DS, p. 48) */
+    CLKPR = (1 << CLKPCE);
+    CLKPR = 0;
+
+    DDRB  |=  _BV(PB7);  	 /* LED as output */
+
+    init_lcd();
+    os_init_scheduler();
+    os_init_ruota();
+}
